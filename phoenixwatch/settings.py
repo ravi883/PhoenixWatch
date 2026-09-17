874e692d9f2cde922ece2ca50c50919b1631b99e
ravi <ravikorat6@gmail.com>
2026-09-17 10:45:59 +0530

    Update tailwind css

--- phoenixwatch/settings.py
+++ phoenixwatch/settings.py
@@ -137,8 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
