26aa28692304f590897f6920900cfd27a5b329c0
ravi <ravikorat6@gmail.com>
2026-09-23 12:28:28 +0530

    Add order history page

--- order/urls.py
+++ order/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("create-order/",create_order_view, name="create-order"),
-    path("order-payment/",order_payment_view, name="order-payment")
+    path("order-payment/",order_payment_view, name="order-payment"),
+    path("order-history/",order_history_view, name="order-history"),
 ]
